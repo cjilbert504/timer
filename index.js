dcaba7318fba5f2071030da5f8802db390750017
Collin Jilbert <cjilbert504@gmail.com>
2020-12-22 09:24:46 -0600

    Add pause method

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ class Timer {
 		this.pauseButton = pauseButton;
 
 		this.startButton.addEventListener('click', this.start);
+		this.pauseButton.addEventListener('click', this.pause);
 	}
 
 	start = () => {
 		this.tick();
-		setInterval(this.tick, 1000);
-	}
+		this.interval = setInterval(this.tick, 1000);
+	};
+
+	pause = () => {
+		clearInterval(this.interval);
+	};
 
 	tick = () => {
 		console.log('tick');
-	}
-}
+	};
+};
 
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
